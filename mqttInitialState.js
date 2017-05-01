e2d0e7da040a42937bb77ed9483f6b3f4c742e8b
Ânderson Ignacio da Silva <anderson@aignacio.com>
2017-05-01 13:18:03 -0300

    Versão de testes de initial state

--- mqttInitialState.js
+++ mqttInitialState.js
@@ -8,8 +8,57 @@ let mac = 'undefined';
 let connected = false;
 let pass_secure = '';
 let portMQTT = 8080;
+let timePublicISIndividual = 1500; // Há DUAS limitações no Initial State, 25k publicações por mês e no máximo uma req. por segundo da API, logo iremos publicar 14 mensagens intervaladas de 1.5 segundos a cada 24 minutos, somando um total de 25.200,00 mensagens por mês aproximadamente
+let timePublicIS = 1000 * 60 * 24;
+let IS = require('initial-state');
+let bucket = IS.bucket('Salas Limpas ITT Chip', 'sOcQGOGPVfYoUIghEs8t8M57KMLVHqEb');
+let sensores6LoWPAN = [];
+let sensorsLength = 0;
+let sensorType = 2;
 
+setTimeout(publicInitialState, timePublicIS);
 
+function publicInitialState() {
+  setTimeout(publicInitialState, timePublicIS);
+
+  sensorType = 2;
+  sensorsLength = sensores6LoWPAN.length;
+  console.log('Enviando dados de temperatura...');
+  setTimeout(publicIndividualIS, timePublicISIndividual);
+}
+
+function publicIndividualIS() {
+  if (sensorType == 2) {
+    sensorsLength = sensorsLength - 1;
+    if (sensorsLength >= 0) {
+      // console.log('Sensor:['+sensorsLength+'] '+sensores6LoWPAN[sensorsLength]);
+      let temp = (Number(String(sensores6LoWPAN[sensorsLength].temp).split('C')[0]) / 100 + 2.1).toFixed(2);
+      temp = String(temp) + ' C';
+      bucket.push(sensores6LoWPAN[sensorsLength].id, temp);
+      setTimeout(publicIndividualIS, timePublicISIndividual);
+      console.log('Enviando ao initial-state os dados: ['+sensores6LoWPAN[sensorsLength].id+'] - '+temp);
+    }
+    else {
+      console.log('Enviando dados de bateria...');
+      sensorsLength = sensores6LoWPAN.length;
+      sensorType = sensorType - 1;
+      setTimeout(publicIndividualIS, timePublicISIndividual);
+    }
+  }
+  else if (sensorType == 1) {
+    sensorsLength = sensorsLength - 1;
+    if (sensorsLength >= 0) {
+      // console.log('Sensor:['+sensorsLength+'] '+sensores6LoWPAN[sensorsLength]);
+      let bat = sensores6LoWPAN[sensorsLength].bat;
+      bucket.push(sensores6LoWPAN[sensorsLength].id, bat);
+      setTimeout(publicIndividualIS, timePublicISIndividual);
+      console.log('Enviando ao initial-state os dados: ['+sensores6LoWPAN[sensorsLength].id+'] - '+bat);
+    }
+    else {
+      sensorType = 0;
+    }
+  }
+}
 
 unique.getMac(function(err, mac_addr) {
   if (err)
@@ -25,13 +74,33 @@ unique.getMac(function(err, mac_addr) {
   });
 
   mqttClient.on('message', function(topic, message) {
-    console.log('[MQTT] Tópico:' + topic + '\n[MQTT] Mensagem:' + message);
+    let dados = String(message).split('|'),
+      newSensor = true,
+      sensor;
+
+    for (var i = 0; i < sensores6LoWPAN.length; i++) {
+      if (sensores6LoWPAN[i].id == dados[0]) {
+        sensores6LoWPAN[i].temp = dados[1];
+        sensores6LoWPAN[i].bat = dados[2];
+        newSensor = false;
+        break;
+      }
+    }
+
+    if (newSensor) {
+      sensor = {
+        'id': dados[0],
+        'temp': dados[1],
+        'bat': dados[2]
+      };
+      sensores6LoWPAN.push(sensor);
+    }
   });
 
   mqttClient.on('connect', function() {
     console.log('Conectado ao broker MQTT!');
     mqttClient.subscribe('#');
-    mqttClient.publish('/' + mac + '/info', 'Publicador de sniffer no initial state ok!');
+    // mqttClient.publish('/' + mac + '/info', 'Publicador de sniffer no initial state ok!');
     connected = true;
   });
 });
